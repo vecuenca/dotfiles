77a9ae96a086760b7708fb490a5b296ede816366
Vincent Cuenca <vincent.cuenca@thescore.com>
2025-08-06 09:15:10 -0400

    stuff

--- nvim/lua/plugins/mini-files.lua
+++ nvim/lua/plugins/mini-files.lua
@@ -3,9 +3,8 @@ return {
     "echasnovski/mini.files",
     opts = {
       windows = {
-        -- Increase preview window size
         preview = true,
-        width_focus = 25, -- Width of focused window
+        width_focus = 30, -- Width of focused window
         width_nofocus = 15, -- Width of non-focused windows
         width_preview = 60, -- Increased preview window width (default is usually 25-30)
       },
--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -3,5 +3,9 @@ return {
   opts = function(_, opts)
     opts.window = opts.window or {}
     opts.window.position = "right"
+
+    opts.filesystem = opts.filesystem or {}
+    opts.filesystem.filtered_items = opts.filesystem.filtered_items or {}
+    opts.filesystem.filtered_items.visible = true
   end,
 }
