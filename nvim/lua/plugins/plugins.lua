575d3f7683b0b7bb735ca153bb7075a791b49f05
Vincent Cuenca <vincent.cuenca@thescore.com>
2025-08-06 09:12:59 -0400

    zenbones time

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -1,91 +1,31 @@
 return {
+  -- {
+  --   "neanias/everforest-nvim",
+  --   version = false,
+  --   lazy = false,
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   -- Optional; default configuration will be used if setup isn't called.
+  --   config = function()
+  --     require("everforest").setup({
+  --       background = "hard", -- or "medium" or "soft"
+  --       transparent_background_level = 2, -- enable transparency
+  --     })
+  --     vim.opt.background = "light"
+  --   end,
+  -- },
   {
-    "neanias/everforest-nvim",
-    version = false,
+    "zenbones-theme/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = "rktjmp/lush.nvim",
     lazy = false,
-    priority = 1000, -- make sure to load this before all the other start plugins
-    -- Optional; default configuration will be used if setup isn't called.
+    priority = 1000,
+    -- you can set set configuration options here
     config = function()
-      require("everforest").setup({
-        background = "hard", -- or "medium" or "soft"
-        transparent_background_level = 2, -- enable transparency
-      })
-      vim.cmd.colorscheme("everforest")
-
-      -- -- Function to apply transparency
-      -- local function apply_transparency()
-      --   local transparent_groups = {
-      --     "Normal",
-      --     "NormalNC",
-      --     "NormalFloat",
-      --     "FloatBorder",
-      --     "TelescopeNormal",
-      --     "TelescopeBorder",
-      --     "TelescopePromptNormal",
-      --     "TelescopeResultsNormal",
-      --     "TelescopePreviewNormal",
-      --     "TelescopePromptBorder",
-      --     "TelescopeResultsBorder",
-      --     "TelescopePreviewBorder",
-      --     "TelescopeTitle",
-      --     "TelescopePromptTitle",
-      --     "TelescopePreviewTitle",
-      --     "TelescopeResultsTitle",
-      --     "SignColumn",
-      --     "EndOfBuffer",
-      --     "StatusLine",
-      --     "StatusLineNC",
-      --     "LineNr",
-      --     "CursorLineNr",
-      --     "Pmenu",
-      --     "PmenuSel",
-      --     "PmenuSbar",
-      --     "PmenuThumb",
-      --     "TabLine",
-      --     "TabLineFill",
-      --     "TabLineSel",
-      --     "WinSeparator",
-      --     "VertSplit",
-      --   }
-      --
-      --   for _, group in ipairs(transparent_groups) do
-      --     vim.api.nvim_set_hl(0, group, { bg = "none" })
-      --   end
-      -- end
-      --
-      -- -- Apply transparency immediately
-      -- apply_transparency()
-      --
-      -- -- Also apply after colorscheme changes
-      -- vim.api.nvim_create_autocmd("ColorScheme", {
-      --   callback = apply_transparency,
-      -- })
-      --
-      -- -- Force telescope transparency after it loads
-      -- vim.api.nvim_create_autocmd("User", {
-      --   pattern = "LazyLoad",
-      --   callback = function(args)
-      --     if args.data == "telescope.nvim" then
-      --       vim.schedule(function()
-      --         -- Force clear all telescope backgrounds
-      --         local telescope_groups = vim.fn.getcompletion("Telescope", "highlight")
-      --         for _, group in ipairs(telescope_groups) do
-      --           vim.api.nvim_set_hl(0, group, { bg = "none" })
-      --         end
-      --       end)
-      --     end
-      --   end,
-      -- })
-      --
-      -- -- Also try when telescope actually opens
-      -- vim.api.nvim_create_autocmd("FileType", {
-      --   pattern = "TelescopePrompt",
-      --   callback = function()
-      --     vim.schedule(function()
-      --       apply_transparency()
-      --     end)
-      --   end,
-      -- })
+      -- vim.g.zenbones_darken_comments = 45
+      vim.g.zenbones_transparent_background = true
+      vim.cmd.colorscheme("zenbones")
     end,
   },
   -- {
@@ -113,7 +53,9 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "everforest",
+      colorscheme = "zenbones",
+
+      -- colorscheme = "everforest",
       -- colorscheme = "catppuccin",
       -- colorscheme = "oldworld",
     },
