67e76437063a1cee2bfa3fb8d64bda7db0941a9e
Vincent Cuenca <vincent.e.cuenca@gmail.com>
2025-12-29 16:37:38 -0500

    rose pine time

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -13,21 +13,91 @@ return {
   --     vim.opt.background = "light"
   --   end,
   -- },
-  -- {
-  --   "zenbones-theme/zenbones.nvim",
-  --   -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-  --   -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-  --   -- In Vim, compat mode is turned on as Lush only works in Neovim.
-  --   dependencies = "rktjmp/lush.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   -- you can set set configuration options here
-  --   config = function()
-  --     -- vim.g.zenbones_darken_comments = 45
-  --     vim.g.zenbones_transparent_background = true
-  --     -- vim.cmd.colorscheme("zenbones")
-  --   end,
-  -- },
+  {
+    "zenbones-theme/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    -- you can set set configuration options here
+    config = function()
+      -- vim.g.zenbones_darken_comments = 45
+      vim.g.zenbones_transparent_background = true
+      -- vim.cmd.colorscheme("zenbones")
+    end,
+  },
+
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("rose-pine").setup({
+        variant = "moon", -- auto, main, moon, or dawn
+        dark_variant = "moon", -- main, moon, or dawn
+        dim_inactive_windows = false,
+        extend_background_behind_borders = true,
+
+        styles = {
+          bold = true,
+          italic = false,
+          transparency = true,
+        },
+
+        groups = {
+          border = "muted",
+          link = "iris",
+          panel = "surface",
+
+          error = "love",
+          hint = "iris",
+          info = "foam",
+          warn = "gold",
+
+          git_add = "foam",
+          git_change = "rose",
+          git_delete = "love",
+          git_dirty = "rose",
+          git_ignore = "muted",
+          git_merge = "iris",
+          git_rename = "pine",
+          git_stage = "iris",
+          git_text = "rose",
+          git_untracked = "subtle",
+
+          headings = {
+            h1 = "iris",
+            h2 = "foam",
+            h3 = "rose",
+            h4 = "gold",
+            h5 = "pine",
+            h6 = "foam",
+          },
+        },
+
+        highlight_groups = {
+          -- Customize line numbers
+          LineNr = { fg = "muted", bold = true },
+          CursorLineNr = { fg = "iris", bold = true },
+
+          -- Make telescope transparent
+          TelescopeBorder = { fg = "overlay", bg = "none" },
+          TelescopeNormal = { fg = "subtle", bg = "none" },
+          TelescopeSelection = { fg = "text", bg = "highlight_med" },
+          TelescopeSelectionCaret = { fg = "love", bg = "highlight_med" },
+          TelescopeMultiSelection = { fg = "text", bg = "highlight_high" },
+          TelescopeTitle = { fg = "iris", bold = true },
+          TelescopePromptTitle = { fg = "base", bg = "love" },
+          TelescopePreviewTitle = { fg = "base", bg = "iris" },
+          TelescopeResultsTitle = { fg = "base", bg = "pine" },
+        },
+      })
+      vim.cmd.colorscheme("rose-pine-moon")
+    end,
+  },
   {
     "catppuccin/nvim",
     lazy = false,
@@ -44,7 +114,8 @@ return {
         },
         transparent_background = true,
       })
-      vim.cmd.colorscheme("catppuccin")
+      -- vim.cmd.colorscheme("zenbones")
+      -- vim.cmd.colorscheme("catppuccin-frappe")
       vim.api.nvim_set_hl(0, "LineNr", { fg = "#a5adce", bold = true })
     end,
   },
